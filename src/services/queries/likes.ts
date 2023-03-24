cab2b5dd600f5ee59a02722043353db9fda21085
Verner Ravil <werner.ravil@gmail.com>
2023-03-24 20:52:54 +0500

    Fixex like and unlike function, added likedItems

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,17 +1,22 @@
 import { client } from '$services/redis';
 import { userLikesKey, itemsKey } from '$services/keys';
+import { getItems } from './items';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
 	return client.sIsMember(userLikesKey(userId), itemId);
 };
 
-export const likedItems = async (userId: string) => {};
+export const likedItems = async (userId: string) => {
+	const ids = await client.sMembers(userLikesKey(userId));
+
+	return getItems(ids);
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
 	const inserted = await client.sAdd(userLikesKey(userId), itemId);
 
 	if (inserted) {
-		await client.hIncrBy(itemsKey(userId), 'likes', 1);
+		return client.hIncrBy(itemsKey(itemId), 'likes', 1);
 	}
 };
 
@@ -19,7 +24,7 @@ export const unlikeItem = async (itemId: string, userId: string) => {
 	const removed = await client.sRem(userLikesKey(userId), itemId);
 
 	if (removed) {
-		await client.hIncrBy(itemsKey(userId), 'likes', -1);
+		return client.hIncrBy(itemsKey(itemId), 'likes', -1);
 	}
 };
 
