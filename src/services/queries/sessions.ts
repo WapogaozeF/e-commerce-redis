b07a9041a6413c510e0a8b472666f34b436dd975
Verner Ravil <werner.ravil@gmail.com>
2023-03-24 11:43:40 +0500

    Added deserialize to session

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -8,9 +8,16 @@ export const getSession = async (id: string) => {
 	if (Object.keys(session).length === 0) {
 		return null;
 	}
-	console.log(session);
 
-	return session;
+	return deserialize(id, session);
 };
 
 export const saveSession = async (session: Session) => {};
+
+const deserialize = (id: string, session: { [key: string]: string }) => {
+	return {
+		id,
+		userId: session.userId,
+		username: session.username
+	};
+};
