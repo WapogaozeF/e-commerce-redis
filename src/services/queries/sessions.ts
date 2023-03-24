d333dd6cde4873cfd3ea1341ea3254352e5caa9b
Verner Ravil <werner.ravil@gmail.com>
2023-03-24 11:42:01 +0500

    Added get session function

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,16 @@
 import type { Session } from '$services/types';
+import { sessionsKey } from '$services/keys';
+import { client } from '$services/redis';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+	const session = await client.hGetAll(sessionsKey(id));
+
+	if (Object.keys(session).length === 0) {
+		return null;
+	}
+	console.log(session);
+
+	return session;
+};
 
 export const saveSession = async (session: Session) => {};
