0558118f28ee1940a9eca0c19c15a872694ac9c0
Verner Ravil <werner.ravil@gmail.com>
2023-07-08 19:38:55 +0500

    fix concurrency problems

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,8 +1,10 @@
 import { randomBytes } from 'crypto';
 import { client } from './client';
+import { build } from '$service-worker';
 
-export const withLock = async (key: string, cb: () => any) => {
+export const withLock = async (key: string, cb: (redisClient: Client, signal: any) => any) => {
 	const retryDelayMs = 100;
+	const timeoutMs = 2000;
 	let retries = 20;
 
 	const token = randomBytes(6).toString('hex');
@@ -14,7 +16,7 @@ export const withLock = async (key: string, cb: () => any) => {
 
 		const acquired = await client.set(lockKey, token, {
 			NX: true,
-			PX: 2000
+			PX: timeoutMs
 		});
 
 		if (!acquired) {
@@ -23,7 +25,13 @@ export const withLock = async (key: string, cb: () => any) => {
 		}
 
 		try {
-			const result = await cb();
+			const signal = { expired: false };
+			setTimeout(() => {
+				signal.expired = true;
+			}, timeoutMs);
+
+			const proxiedClient = buildClientProxy(timeoutMs);
+			const result = await cb(proxiedClient, signal);
 			return result;
 		} finally {
 			await client.unlock(lockKey, token);
@@ -31,7 +39,23 @@ export const withLock = async (key: string, cb: () => any) => {
 	}
 };
 
-const buildClientProxy = () => {};
+type Client = typeof client;
+const buildClientProxy = (timeoutMs: number) => {
+	const startTime = Date.now();
+
+	const handler = {
+		get(target: Client, prop: keyof Client) {
+			if (Date.now() >= startTime + timeoutMs) {
+				throw new Error('Lock has expired.');
+			}
+
+			const value = target[prop];
+			return typeof value === 'function' ? value.bind(target) : value;
+		}
+	};
+
+	return new Proxy(client, handler) as Client;
+};
 
 const pause = (duration: number) => {
 	return new Promise((resolve) => {
