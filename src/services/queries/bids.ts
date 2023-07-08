a9df06dadc1dba721023eebd32020d5d820b0d0f
Verner Ravil <werner.ravil@gmail.com>
2023-07-08 10:45:56 +0500

    Isolated execute via withLock

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,40 +1,73 @@
 import type { CreateBidAttrs } from '$services/types';
 import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	return client.executeIsolated(async (isolatedClient) => {
-		await isolatedClient.watch(itemsKey(attrs.itemId));
+	return withLock(attrs.itemId, async () => {
+		return client.executeIsolated(async (isolatedClient) => {
+			await isolatedClient.watch(itemsKey(attrs.itemId));
 
-		const item = await getItem(attrs.itemId);
+			const item = await getItem(attrs.itemId);
 
-		if (!item) {
-			throw new Error('Item does not exist');
-		}
-		if (item.price >= attrs.amount) {
-			throw new Error('Bid too low');
-		}
-		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
-			throw new Error('Item closed to bidding');
-		}
+			if (!item) {
+				throw new Error('Item does not exist');
+			}
+			// if (item.price >= attrs.amount) {
+			// 	throw new Error('Bid too low');
+			// }
+			if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+				throw new Error('Item closed to bidding');
+			}
 
-		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+			const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
 
-		return isolatedClient
-			.multi()
-			.rPush(bidHistoryKey(attrs.itemId), serialized)
-			.hSet(itemsKey(item.id), {
-				bids: item.bids + 1,
-				price: attrs.amount,
-				highestBidUserId: attrs.userId
-			})
-			.zAdd(itemsByPriceKey(), {
-				value: item.id,
-				score: attrs.amount
-			})
-			.exec();
+			return Promise.all([
+				client.rPush(bidHistoryKey(attrs.itemId), serialized),
+				client.hSet(itemsKey(item.id), {
+					bids: item.bids + 1,
+					price: attrs.amount,
+					highestBidUserId: attrs.userId
+				}),
+				client.zAdd(itemsByPriceKey(), {
+					value: item.id,
+					score: attrs.amount
+				})
+			]);
+		});
+
+		// return client.executeIsolated(async (isolatedClient) => {
+		// 	await isolatedClient.watch(itemsKey(attrs.itemId));
+
+		// 	const item = await getItem(attrs.itemId);
+
+		// 	if (!item) {
+		// 		throw new Error('Item does not exist');
+		// 	}
+		// 	if (item.price >= attrs.amount) {
+		// 		throw new Error('Bid too low');
+		// 	}
+		// 	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+		// 		throw new Error('Item closed to bidding');
+		// 	}
+
+		// 	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+		// 	return isolatedClient
+		// 		.multi()
+		// 		.rPush(bidHistoryKey(attrs.itemId), serialized)
+		// 		.hSet(itemsKey(item.id), {
+		// 			bids: item.bids + 1,
+		// 			price: attrs.amount,
+		// 			highestBidUserId: attrs.userId
+		// 		})
+		// 		.zAdd(itemsByPriceKey(), {
+		// 			value: item.id,
+		// 			score: attrs.amount
+		// 		})
+		// 		.exec();
+		// });
 	});
 };
 
